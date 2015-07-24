c5142b59da8adc02b5b29aa46e0243bf5d7a5891
Austin Hibdon <austin.hibdon@gmail.com>
2015-07-24 12:09:48 -0400

    adding the links at top

--- sites/all/themes/bartik_plus/template.php
+++ sites/all/themes/bartik_plus/template.php
@@ -4,7 +4,7 @@ function bartik_plus_links($variables) {
   if (array_key_exists('id', $variables['attributes']) && $variables['attributes']['id'] == 'main-menu-links') {
   	$pid = variable_get('menu_main_links_source', 'main-menu');
 	$tree = menu_tree($pid);
-	return '<div id="main-menu-links" style="background-color: cyan;">' . drupal_render($tree) . "</div>";
+	return '<div id="inter-links"><a href="ind.inv.ott-toolbox.com">INDUSTRIAL</a> <a href="com.inv.ott-toolbox.com">COMMERCIAL</a> <a href="res.inv.ott-toolbox.com">RESIDENTIAL</a></div>\n<div id="main-menu-links">' . drupal_render($tree) . "</div>";
   }
   return theme_links($variables);
 }
