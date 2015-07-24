91113e7345214044c32bfdb6eb8f8366d77c60e0
Austin Hibdon <austin.hibdon@gmail.com>
2015-07-24 12:23:23 -0400

    navigation should be good now.

--- sites/all/themes/bartik_plus/template.php
+++ sites/all/themes/bartik_plus/template.php
@@ -4,7 +4,7 @@ function bartik_plus_links($variables) {
   if (array_key_exists('id', $variables['attributes']) && $variables['attributes']['id'] == 'main-menu-links') {
   	$pid = variable_get('menu_main_links_source', 'main-menu');
 	$tree = menu_tree($pid);
-	return '<div id="inter-links"><a id="ind" href="ind.inv.ott-toolbox.com">INDUSTRIAL</a> <a id="com" href="com.inv.ott-toolbox.com">COMMERCIAL</a> <a id="res" href="res.inv.ott-toolbox.com">RESIDENTIAL</a></div>\n<div id="main-menu-links">' . drupal_render($tree) . "</div>";
+	return '<div id="inter-links"><a id="ind" href="ind.inv.ott-toolbox.com">INDUSTRIAL</a> <a id="com" href="com.inv.ott-toolbox.com">COMMERCIAL</a> <a id="res" href="res.inv.ott-toolbox.com">RESIDENTIAL</a></div><div id="main-menu-links">' . drupal_render($tree) . "</div>";
   }
   return theme_links($variables);
 }
